4b8a1e9bfc0c07e8968dc12cf47919175ed234be
Gordon Cheung <porkguy69@Gordons-MacBook-Air.local>
2024-11-26 15:46:13 +0000

    something

--- backend_golang/transactions/manage_transactions.go
+++ backend_golang/transactions/manage_transactions.go
@@ -1,2 +1,37 @@
 package transactions
 
+import (
+	"log"
+	"net/http"
+)
+
+func AddTransaction() {
+
+}
+
+func RemoveTransaction() {
+
+}
+
+func UpdateTransaction() {
+
+}
+
+func GetTransaction() {
+
+}
+
+func TransactionHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "transactions/":
+		GetTransaction()
+	case "transactions/add_transaction":
+		AddTransaction()
+	case "transactions/remove_transaction":
+		RemoveTransaction()
+	case "transactions/update_transaction":
+		UpdateTransaction()
+	default:
+		log.Fatal("Something went wrong with the URL.")
+	}
+}
