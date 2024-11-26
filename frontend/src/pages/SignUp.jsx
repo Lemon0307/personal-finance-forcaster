ca4d80b424deac71329824ab36ea39b3b52abb1f
Gordon Cheung <porkguy69@Gordons-MacBook-Air.local>
2024-11-26 23:05:44 +0000

    something

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,20 @@
+const SignUp = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="Username..."/>
+                <input type="text" placeholder="Email..."/>
+                <input type="text" placeholder="Password..."/>
+                <input type="text" placeholder="Confirm Password..."/>
+                <input type="text" placeholder="Forename..."/>
+                <input type="text" placeholder="Surname..."/>
+                <input type="text" placeholder="Date Of Birth..."/>
+                <input type="text" placeholder="Address..."/>
+                <input type="text" placeholder="Current Balance..."/>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+export {default as Home} from "./Home";
+export {default as Forecast} from "./Forecast";
+export {default as Budgets} from "./Budgets";
+export {default as Login} from "./Login";
+export {default as SignUp} from "./SignUp";
+export {default as Transactions} from "./Transactions";
