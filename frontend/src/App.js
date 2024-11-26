59a3504808acc48f454a74826325c6297a91bd2d
Gordon Cheung <porkguy69@Gordons-MacBook-Air.local>
2024-11-26 23:05:44 +0000

    something

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Home, Forecast, Budgets, Login, SignUp, Transactions} from './pages';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
-        <Route path="/sign-up"/>
-        <Route path="/login" />
-        <Route path="/budgets" />
-        <Route path="/transactions" />
-        <Route path="/forecast" />
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/budgets" element={<Budgets />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/forecast" element={<Forecast />} />
       </Routes>
     </Router>
   );
