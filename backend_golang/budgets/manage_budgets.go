232c93767a2034d31836d080cac53c33a68b0125
Gordon Cheung <porkguy69@Gordons-MacBook-Air.local>
2024-11-26 15:22:07 +0000

    something

--- backend_golang/budgets/manage_budgets.go
+++ backend_golang/budgets/manage_budgets.go
@@ -1,9 +1,37 @@
 package budgets
 
 import (
+	"log"
 	"net/http"
 )
 
-func BudgetHandler(w http.ResponseWriter, r *http.Request) {
+func AddBudget() {
 	
+}
+
+func RemoveBudget() {
+
+}
+
+func UpdateBudget() {
+
+}
+
+func GetBudget() {
+
+}
+
+func BudgetHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "budgets/":
+		GetBudget()
+	case "budgets/add_budget":
+		AddBudget()
+	case "budgets/remove_budget":
+		RemoveBudget()
+	case "budgets/update_budget":
+		UpdateBudget()
+	default:
+		log.Fatal("Something went wrong going to a URL.")
+	}
 }
